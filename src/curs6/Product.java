0f9b7053d39079cac97a9a5e1163f0489c960e96
Daniela <stoicadana@hotmail.com>
2022-09-03 14:51:57 +0300

    Commit hw6

--- src/curs6/Product.java
+++ src/curs6/Product.java
@@ -23,18 +23,11 @@ public class Product {
     }
 
     public boolean hasStock(int quantity) {
-        if (quantity > 0) {
-            return true;
-        }
-        return false;
+        return quantity > 0;
     }
 
     public boolean isCategory(String categ){
-        if (this.category.equals(categ)){
-            return true;
-        }
-        return false;
-
+        return this.category.equals(categ);
     }
 
     Product(String name, float price, int quantity, String category){
@@ -48,14 +41,18 @@ public class Product {
         Product p1 = new Product("Violeta", (float) 42.2, 10, "Literatura");
         Product p2 = new Product("Matematica clasa a II-a", (float) 22.2, 12, "Manuale");
         Product p3 = new Product("Matematica clasa a III-a", (float) 25.2, 5, "Manuale");
+        Product p4 = new Product("Matematica clasa a IV-a", (float) 25.2, 0, "Manuale");
 
-        System.out.println("Product 1: "+p1.getName());
-        System.out.println("Product 2: "+p2.getName());
-        System.out.println("Product 3: "+p3.getName());
+        System.out.println("Product 1: "+p1.getName()+" pret "+p1.getPrice());
+        System.out.println("Product 2: "+p2.getName()+" pret "+p2.getPrice());
+        System.out.println("Product 3: "+p3.getName()+" pret "+p3.getPrice());
+        System.out.println("Product 3: "+p4.getName()+" pret "+p4.getPrice());
 
         if (p2.isCategory(p3.getCategory())) {
-            System.out.println("Produsul "+p1.getName()+" si produsul "+p2.getName() + " au aceeasi categorie");
+            System.out.println("Produsul "+p2.getName()+" si produsul "+p3.getName() + " au aceeasi categorie");
         }
+
+        System.out.println("Produsul "+(p4.hasStock(p4.getQuantity())? p4.getName()+" este in stoc": p4.getName()+" nu este in stoc"));
 
     }
 
